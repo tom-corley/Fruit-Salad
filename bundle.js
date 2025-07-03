43a376335820179f82cb2a5418ff47d0bffb29e0
Tom Corley <tomcorley86@gmail.com>
2025-07-03 15:31:14 +0100

    Added API functionality.

--- bundle.js
+++ bundle.js
@@ -10,9 +10,41 @@ fruitForm.addEventListener(
     extractFruit
 );
 
+const FruitList = document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p");
+
+// e for event here
 function extractFruit(e) {
     e.preventDefault();
-    console.log(e);
+    new_fruit = e.target.fruitInput.value;
+    if (new_fruit == "") {
+        return;
+    }
+    fetchFruitData(new_fruit);
+    return;
+}
+let cal = 0;
+function addFruit(fruit) {
+    const li = document.createElement("li");
+    li.addEventListener(
+        "click",
+        () => li.remove()
+    )
+    li.textContent = fruit.name;
+    FruitList.appendChild(li);
+
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = "Total Calories: " + cal;
+}
+
+function fetchFruitData(fruit) {
+    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+    // returns promise
+        .then((resp) => resp.json())
+        // convert the response into a json
+        .then(data => addFruit(data))
+        // addFruit by extracting field from the json
+        .catch((e) => console.log(e))
 }
 },{"./script2.js":2}],2:[function(require,module,exports){
 const x = 42;
--- script.js
+++ script.js
@@ -9,7 +9,39 @@ fruitForm.addEventListener(
     extractFruit
 );
 
+const FruitList = document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p");
+
+// e for event here
 function extractFruit(e) {
     e.preventDefault();
-    console.log(e);
+    new_fruit = e.target.fruitInput.value;
+    if (new_fruit == "") {
+        return;
+    }
+    fetchFruitData(new_fruit);
+    return;
+}
+let cal = 0;
+function addFruit(fruit) {
+    const li = document.createElement("li");
+    li.addEventListener(
+        "click",
+        () => li.remove()
+    )
+    li.textContent = fruit.name;
+    FruitList.appendChild(li);
+
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = "Total Calories: " + cal;
+}
+
+function fetchFruitData(fruit) {
+    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+    // returns promise
+        .then((resp) => resp.json())
+        // convert the response into a json
+        .then(data => addFruit(data))
+        // addFruit by extracting field from the json
+        .catch((e) => console.log(e))
 }
