a22ec0f27068d059e8fa764883e5208394351a59
Tom Corley <tomcorley86@gmail.com>
2025-07-15 16:13:47 +0100

    Added some stuff.

--- bundle.js
+++ bundle.js
@@ -1,63 +1,79 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const x = require('./script2');
+const fruitForm = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
-const fruitNutrition = document.querySelector("#nutritionSection");
-const fruitFrom = document.querySelector("#inputSection form");
-fruitFrom.addEventListener("submit", extractFruit);
-function extractFruit(e){
+const fruitNutrition = document.querySelector("#nutritionSection p");
+const createForm = document.querySelector("#create-form");
+let cal = 0;
+const fruitCal = {};
+const apiKey = "50435851-73f213f32ab40e115e4c505e2";
+fruitForm.addEventListener("submit", extractFruit);
+createForm.addEventListener("submit", createNewFruit);
+function extractFruit(e) {
     e.preventDefault();
     fetchFruitData(e.target.fruitInput.value);
     e.target.fruitInput.value = "";
 }
- let cal = 0;
-function addFruit(fruit){
-    const li = document.createElement("li");
-    li.textContent = fruit.name;
-    li.dataset.calories = fruit.nutritions.calories; // Store calories in the element's dataset
-    li.addEventListener("click", (e) => removeFruitCalories(e)); // Add click event listener to delete the item when clicked
-    fruitList.appendChild(li);
-    // Create image element for this specific fruit
-    const liImage = document.createElement("img");
-    liImage.style.width = "50px";
-    liImage.style.height = "50px";
-    liImage.style.objectFit = "cover";
-    liImage.style.marginRight = "10px";
-    liImage.style.borderRadius = "4px";
-    // Fetch and display the fruit image
-    fetchFruitImage(fruit.name, liImage, li);
-    cal += fruit.nutritions.calories;
-    nutritionSection.textContent = "The current calorie count is: " + cal;
-}
-function removeFruitCalories(e){
-    // Get the list item element (either the clicked element or its parent)
-    const listItem = e.target.tagName === 'LI' ? e.target : e.target.closest('li');
-    const fruitCalories = parseInt(listItem.dataset.calories);
-    cal -= fruitCalories; // Subtract the calories from the total
-    nutritionSection.textContent = "The current calorie count is: " + cal; // Update the display
-    listItem.remove(); // Remove the entire list item
-}
-function fetchFruitData(fruit) {
-    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`) //returns a promise
-        .then((resp) => resp.json()) //converts response to JSON
-        .then(data => addFruit(data))
-        .catch((e) => console.log(e));
-}
-function fetchFruitImage(fruit, imageElement, listItem) {
-    fetch(`https://api.pexels.com/v1/search?query=${fruit}&per_page=1`, {
-        headers: {
-            Authorization: 'xnTYNA70Tn02biufBTuo25RcUESEXwPExGFeCeOC57SQaZaxvyhmRY7u'
+async function fetchFruitData(fruit) {
+    try {
+        //Make sure to replace this link with your deployed API URL in this fetch
+        const respData = await fetch(`https://fruit-7tpx.onrender.com/fruits/${fruit}`);
+        const respImg = await fetch(
+            `https://pixabay.com/api/?q=${fruit}+fruit&key=${apiKey}`
+        );
+        if (respData.ok && respImg.ok) {
+            const data = await respData.json();
+            const imgData = await respImg.json();
+            addFruit(data, imgData);
+        } else {
+            throw "Something has gone wrong with one of the API requests";
         }
-    })
-        .then((resp) => resp.json())
-        .then(data => {
-            if (data.photos && data.photos.length > 0) {
-                imageElement.src = data.photos[0].src.small;
-                imageElement.alt = fruit;
-                listItem.prepend(imageElement); // Add image to the beginning of the list item
-            }
-        })
-        .catch((e) => console.error(e));
+    } catch (e) {
+        console.log(e);
+    }
 }
-},{"./script2":2}],2:[function(require,module,exports){
-
+async function createNewFruit(e) {
+    e.preventDefault();
+    const data = {name: e.target.fruitInput.value};
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    //Make sure to add your deployed API URL in this fetch
+    const response = await fetch(`https://fruit-salad-backend-lyej.onrender.com/fruits`, options);
+    let messageStatus = document.querySelector("#message")
+    if(response.status === 201) {
+        e.target.fruitInput.value = ''
+        messageStatus.textContent = "Fruit successfully created."
+        setTimeout(() => {
+          messageStatus.textContent = ""
+        }, 4000)
+    } else {
+        e.target.fruitInput.value = ''
+        messageStatus.textContent = "This fruit already exists. Please enter another fruit!"
+        setTimeout(() => {
+          messageStatus.textContent = ""
+        }, 4000)
+    }
+}
+function addFruit(fruit, fruitImg) {
+    const img = document.createElement("img");
+    img.classList.add('fruits');
+    img.alt = fruit.name;
+    img.src = fruitImg.hits[0].previewURL;
+    img.addEventListener("click", removeFruit, { once: true });
+    fruitList.appendChild(img);
+    fruitCal[fruit.name] = fruit.nutritions.calories;
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = "Total Calories: " + cal;
+}
+function removeFruit(e) {
+    const fruitName = e.target.alt;
+    cal -= fruitCal[fruitName];
+    fruitNutrition.textContent = "Total Calories: " + cal;
+    delete fruitCal[fruitName];
+    e.target.remove();
+}
 },{}]},{},[1]);
